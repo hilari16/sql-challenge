05e49b74af1ca3520d0e702612b2464a61545165
hilari16 <hilari.woodard@yahoo.com>
2024-01-24 18:24:13 -0600

    Start on Schemas

---
+++ EmployeeSQL/table_schema.sql
@@ -0,0 +1,39 @@
+CREATE TABLE departments (
+	dept_no VARCHAR NOT NULL,
+	dept_name VARCHAR NOT NULL
+);
+
+
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL
+);
+
+
+CREATE TABLE dept_manager (
+	dept_no VARCHAR NOT NULL,
+	emp_no INT NOT NULL
+);
+
+
+CREATE TABLE employees (
+	emp_no INT NOT NULL,
+	emp_title_id VARCHAR NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	sex VARCHAR NOT NULL,
+	hire_date DATE NOT NULL
+);
+
+
+CREATE TABLE salaries (
+	emp_no INT NOT NULL,
+	salary INT NOT NULL
+);
+
+
+CREATE TABLE titles (
+	title_id VARCHAR NOT NULL,
+	title VARCHAR NOT NULL
+);
